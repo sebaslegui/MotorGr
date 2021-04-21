a2a8d12ce333d81f798a9e8cdffa1cc1939c8975
sebaslegui <71037285+sebaslegui@users.noreply.github.com>
2021-04-21 17:33:18 -0300

    Primer commit

---
+++ Assets/ExampleWindows.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExampleWindows : EditorWindow
+{
+    Color color;
+
+    [MenuItem("Ventana/Color")]
+   public static void ShowWindow ()
+    {
+        GetWindow<ExampleWindows>("Color");
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Seccion para insertar color.", EditorStyles.boldLabel);
+
+        color = EditorGUILayout.ColorField("Color", color);
+
+        if (GUILayout.Button("INSERTAR COLOR"))
+        {
+            foreach (GameObject obj in Selection.gameObjects)
+            {
+                Renderer renderer = obj.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    renderer.sharedMaterial.color = color;
+                }
+            }
+           
+        }
+    }
+
+
+ 
+
+}
